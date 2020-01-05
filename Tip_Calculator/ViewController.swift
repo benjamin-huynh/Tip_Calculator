3cf2f35965ca1e5062ebe2cb9756a5374586a046
Benjamin Huynh <ben.huynh408@gmail.com>
2020-01-05 14:00:40 -0800

    Initial Commit

---
+++ Tip_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip_Calculator
+//
+//  Created by Ben Huynh on 1/5/20.
+//  Copyright © 2020 Ben Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
